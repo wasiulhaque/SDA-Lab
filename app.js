6e95a99ff2971bfbbf240a570b97bbac43f7ea12
Wasiul Haque <wasiulhq@gmail.com>
2021-12-08 12:02:53 +0600

    Changed app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res) => {
-    res.send("Welcome Heroku")
+    res.send("Welcome to Heroku Deployment.")
 })
 
 app.listen(8000,() => {
